cd25a589edfb0c5988dd61f3b983043a529371b3
Buddhika Prasanna <kbuddhika89@gmail.com>
2019-01-01 16:30:42 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="/bootstrap/js/jquery.min.js"></script>
+    <script src="/bootstrap/js/popper.min.js"></script>
+    <script src="/bootstrap/js/tooltip.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+
+    <title>Home Page</title>
+</head>
+<body>
+
+<div class="container-fluid">
+    <h1>Hello World!</h1>
+    <p>Three equal width columns! Try to add a new div with class="col" inside the row class - this will create four equal-width columns.</p>
+    <div class="row">
+        <div class="col" style="background-color:lavender;">.col</div>
+        <div class="col" style="background-color:orange;">.col</div>
+        <div class="col" style="background-color:lavender;">.col</div>
+    </div>
+</div>
+
+<?php
+echo 'Hello World!';
+?>
+
+</body>
+</html>
