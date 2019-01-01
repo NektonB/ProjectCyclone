d9475467561894d330e4a9e058ea1c8769d8c935
Buddhika Prasanna <kbuddhika89@gmail.com>
2019-01-01 18:06:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <!--Link for bootstrap-->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <script src="/bootstrap/js/jquery.min.js"></script>
